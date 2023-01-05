9a7430165e806e4a0bf73f2a8fb6aeed985fa268
flemzord <flemzord@users.noreply.github.com>
2023-01-05 09:42:24 +0000

    feat: branch feat/add_search_in_monorepo

--- Formance/paths/api_webhooks_configs_id/delete.pyi
+++ Formance/paths/api_webhooks_configs_id/delete.pyi
@@ -67,7 +67,7 @@ _response_for_200 = api_client.OpenApiResponse(
 
 class BaseApi(api_client.Api):
     @typing.overload
-    def _delete_one_config_oapg(
+    def _delete_config_oapg(
         self,
         path_params: RequestPathParams = frozendict.frozendict(),
         stream: bool = False,
@@ -78,7 +78,7 @@ class BaseApi(api_client.Api):
     ]: ...
 
     @typing.overload
-    def _delete_one_config_oapg(
+    def _delete_config_oapg(
         self,
         skip_deserialization: typing_extensions.Literal[True],
         path_params: RequestPathParams = frozendict.frozendict(),
@@ -87,7 +87,7 @@ class BaseApi(api_client.Api):
     ) -> api_client.ApiResponseWithoutDeserialization: ...
 
     @typing.overload
-    def _delete_one_config_oapg(
+    def _delete_config_oapg(
         self,
         path_params: RequestPathParams = frozendict.frozendict(),
         stream: bool = False,
@@ -98,7 +98,7 @@ class BaseApi(api_client.Api):
         api_client.ApiResponseWithoutDeserialization,
     ]: ...
 
-    def _delete_one_config_oapg(
+    def _delete_config_oapg(
         self,
         path_params: RequestPathParams = frozendict.frozendict(),
         stream: bool = False,
@@ -155,11 +155,11 @@ class BaseApi(api_client.Api):
         return api_response
 
 
-class DeleteOneConfig(BaseApi):
+class DeleteConfig(BaseApi):
     # this class is used by api classes that refer to endpoints with operationId fn names
 
     @typing.overload
-    def delete_one_config(
+    def delete_config(
         self,
         path_params: RequestPathParams = frozendict.frozendict(),
         stream: bool = False,
@@ -170,7 +170,7 @@ class DeleteOneConfig(BaseApi):
     ]: ...
 
     @typing.overload
-    def delete_one_config(
+    def delete_config(
         self,
         skip_deserialization: typing_extensions.Literal[True],
         path_params: RequestPathParams = frozendict.frozendict(),
@@ -179,7 +179,7 @@ class DeleteOneConfig(BaseApi):
     ) -> api_client.ApiResponseWithoutDeserialization: ...
 
     @typing.overload
-    def delete_one_config(
+    def delete_config(
         self,
         path_params: RequestPathParams = frozendict.frozendict(),
         stream: bool = False,
@@ -190,14 +190,14 @@ class DeleteOneConfig(BaseApi):
         api_client.ApiResponseWithoutDeserialization,
     ]: ...
 
-    def delete_one_config(
+    def delete_config(
         self,
         path_params: RequestPathParams = frozendict.frozendict(),
         stream: bool = False,
         timeout: typing.Optional[typing.Union[int, typing.Tuple]] = None,
         skip_deserialization: bool = False,
     ):
-        return self._delete_one_config_oapg(
+        return self._delete_config_oapg(
             path_params=path_params,
             stream=stream,
             timeout=timeout,
@@ -247,7 +247,7 @@ class ApiFordelete(BaseApi):
         timeout: typing.Optional[typing.Union[int, typing.Tuple]] = None,
         skip_deserialization: bool = False,
     ):
-        return self._delete_one_config_oapg(
+        return self._delete_config_oapg(
             path_params=path_params,
             stream=stream,
             timeout=timeout,
