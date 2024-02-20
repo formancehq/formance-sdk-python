b67ab66883fc8175f9908d7524ca10f0a90e51f7
NumaryBot <support@formance.com>
2024-02-20 18:54:02 +0000

    release(sdk): v2.0.0-rc.10

--- src/sdk/sdkconfiguration.py
+++ src/sdk/sdkconfiguration.py
@@ -21,10 +21,10 @@ class SDKConfiguration:
     server_url: str = ''
     server_idx: int = 0
     language: str = 'python'
-    openapi_doc_version: str = 'v2.0.0-rc.9'
-    sdk_version: str = 'v2.0.0-rc.9'
+    openapi_doc_version: str = 'v2.0.0-rc.10'
+    sdk_version: str = 'v2.0.0-rc.10'
     gen_version: str = '2.237.2'
-    user_agent: str = 'speakeasy-sdk/python v2.0.0-rc.9 2.237.2 v2.0.0-rc.9 formance-sdk-python'
+    user_agent: str = 'speakeasy-sdk/python v2.0.0-rc.10 2.237.2 v2.0.0-rc.10 formance-sdk-python'
     retry_config: RetryConfig = None
 
     def get_server_details(self) -> Tuple[str, Dict[str, str]]:
